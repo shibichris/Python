4ccc485f86de3ab71554e7622552a9cfc9f16f2b
shibichris <99465215+shibichris@users.noreply.github.com>
2022-06-13 08:29:36 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,20 @@
+ x = 'SHIBIN'
+
+ for i in x:
+    print(i)
+
+ for i in [2,6,'Paul']:
+     print(i)
+
+ for i in range(10):
+     print(i)
+
+ for i in range(11,21,5):
+     print(i)
+
+ for i in range(20,10,-1):
+     print(i)
+
+for i in range(1,21):
+    if i%5!=0:
+        print(i)
