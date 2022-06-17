0ecd8a5d5488b36ac9832a814250cc68b619950e
shibichris <99465215+shibichris@users.noreply.github.com>
2022-06-17 16:48:39 +0530

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -18,3 +18,17 @@
 for i in range(1,21):
     if i%5!=0:
         print(i)
+    
+    # (While loop)
+    
+    i = 1
+
+while i<=5:
+    print("Telusko ",end="")
+    j=1
+    while j<=4:
+        print("Rocks ",end="")
+        j=j+1
+
+    i=i+1
+    print()
