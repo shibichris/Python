53c2304285766ad94a7a51e4fa68a9138648ed2f
shibichris <99465215+shibichris@users.noreply.github.com>
2022-06-21 10:14:36 +0530

    Update InheritanceAnimal.py

--- InheritanceAnimal.py
+++ InheritanceAnimal.py
@@ -1,10 +1,15 @@
-class Animal:
-    def speak(self):
-        print("Animal Speaking")
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
 
-class Dog(Animal):
-    def bark(self):
-        print("dog barking")
-d = Dog()
-d.bark()
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+ 
+class DogChild(Dog):  
+    def eat(self):  
+        print("Eating bread...")  
+d = DogChild()  
+d.bark()  
 d.speak()  
+d.eat()  
